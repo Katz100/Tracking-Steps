1bcb847e446757aa4f70ef331d239f183ac3fa29
Katz100 <cody.hopkins108@gmail.com>
2026-03-14 20:37:43 -0700

    hello

--- app/src/main/java/com/example/tracking_steps/MainActivity.kt
+++ app/src/main/java/com/example/tracking_steps/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TrackingStepsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hello World",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
