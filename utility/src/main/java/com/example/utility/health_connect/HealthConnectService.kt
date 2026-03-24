b6d38b35acbbf71e43bf5af5c932a9887961c3c6
Katz100 <cody.hopkins108@gmail.com>
2026-03-24 12:33:17 -0700

    Read/write calories to health connect

--- utility/src/main/java/com/example/utility/health_connect/HealthConnectService.kt
+++ utility/src/main/java/com/example/utility/health_connect/HealthConnectService.kt
@@ -9,6 +9,7 @@ import androidx.health.connect.client.PermissionController
 import androidx.health.connect.client.permission.HealthPermission
 import androidx.health.connect.client.records.ExerciseSessionRecord
 import androidx.health.connect.client.records.StepsRecord
+import androidx.health.connect.client.records.TotalCaloriesBurnedRecord
 import androidx.health.connect.client.records.WeightRecord
 import androidx.health.connect.client.records.metadata.Device
 import androidx.health.connect.client.records.metadata.Metadata
@@ -16,6 +17,7 @@ import androidx.health.connect.client.request.AggregateRequest
 import androidx.health.connect.client.request.ReadRecordsRequest
 import androidx.health.connect.client.response.InsertRecordsResponse
 import androidx.health.connect.client.time.TimeRangeFilter
+import androidx.health.connect.client.units.Energy
 import androidx.health.connect.client.units.Mass
 import java.time.Instant
 import java.time.ZoneOffset
@@ -33,6 +35,8 @@ class HealthConnectService @Inject constructor(
                 HealthPermission.Companion.getWritePermission(StepsRecord::class),
                 HealthPermission.Companion.getReadPermission(WeightRecord::class),
                 HealthPermission.Companion.getWritePermission(WeightRecord::class),
+                HealthPermission.getReadPermission(TotalCaloriesBurnedRecord::class),
+                HealthPermission.getWritePermission(TotalCaloriesBurnedRecord::class),
             )
 
         fun isActivityRecognitionGranted(context: Context): Boolean {
@@ -98,6 +102,26 @@ class HealthConnectService @Inject constructor(
         }
     }
 
+    suspend fun readCaloriesAggregate(
+        startTime: Instant,
+        endTime: Instant,
+        onError: (String) -> Unit
+    ): Energy? {
+        return try {
+            val response = healthConnectClient.aggregate(
+                AggregateRequest(
+                    metrics = setOf(TotalCaloriesBurnedRecord.ENERGY_TOTAL),
+                    timeRangeFilter = TimeRangeFilter.Companion.between(startTime, endTime)
+                )
+            )
+            // The result may be null if no data is available in the time range
+            response[TotalCaloriesBurnedRecord.ENERGY_TOTAL]
+        } catch (e: Exception) {
+            onError(e.message.toString())
+            null
+        }
+    }
+
     suspend fun writeWeightInput(weightInput: Double): InsertRecordsResponse? {
         val time = ZonedDateTime.now().withNano(0)
         val weightRecord = WeightRecord(
@@ -107,6 +131,29 @@ class HealthConnectService @Inject constructor(
             zoneOffset = time.offset
         )
         val records = listOf(weightRecord)
+        return try {
+            healthConnectClient.insertRecords(records)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    suspend fun writeCaloriesBurned(
+        calories: Double,
+        startTime: Instant,
+        endTime: Instant,
+    ): InsertRecordsResponse? {
+        val time = ZonedDateTime.now().withNano(0)
+
+        val caloriesRecord = TotalCaloriesBurnedRecord(
+            startTime = startTime,
+            endTime = endTime,
+            startZoneOffset = time.offset,
+            endZoneOffset = null,
+            energy = Energy.calories(calories),
+            metadata = Metadata.manualEntry()
+        )
+        val records = listOf(caloriesRecord)
         return try {
             healthConnectClient.insertRecords(records)
         } catch (e: Exception) {
