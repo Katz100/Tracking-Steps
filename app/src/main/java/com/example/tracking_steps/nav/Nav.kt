6fb1179daa202cd9c4062b7aa720b4deafb4b2db
Katz100 <cody.hopkins108@gmail.com>
2026-04-22 11:55:59 -0700

    Ask user for confirmation before ending session

--- app/src/main/java/com/example/tracking_steps/nav/Nav.kt
+++ app/src/main/java/com/example/tracking_steps/nav/Nav.kt
@@ -7,21 +7,19 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -31,10 +29,11 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.feature_home.HomePage
 import com.example.goals.GoalsPage
-import com.example.utility.composables.ValueStepper
+import com.example.utility.composables.CustomAlertDialog
 import com.example.utility.foreground.SessionState
 import com.example.utility.foreground.StepTrackingService
 import timber.log.Timber
+import com.example.utility.R
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
@@ -93,6 +92,23 @@ fun Nav(
             startDestination = Screen.Home
         ) {
             composable<Screen.Home> {
+                val openAlertDialog = remember { mutableStateOf(false) }
+                when {
+                    openAlertDialog.value -> {
+                        CustomAlertDialog(
+                            onDismissRequest = { openAlertDialog.value = false },
+                            onConfirmation = {
+                                openAlertDialog.value = false
+                                val intent = Intent(context, StepTrackingService::class.java)
+                                context.stopService(intent)
+                            },
+                            dialogTitle = "End Session",
+                            dialogText = "Are you sure you want to end your walking session?",
+                            icon = painterResource(R.drawable.walking)
+                        )
+                    }
+                }
+
                 HomePage(
                     onLogFoodClicked = onLogFoodClicked,
                     onStartWalkClicked = { weight, stepGoal, sessionState ->
@@ -106,8 +122,7 @@ fun Nav(
                                 }
                             context.startForegroundService(intent)
                         } else {
-                            val intent = Intent(context, StepTrackingService::class.java)
-                            context.stopService(intent)
+                            openAlertDialog.value = true
                         }
                     }
                 )
