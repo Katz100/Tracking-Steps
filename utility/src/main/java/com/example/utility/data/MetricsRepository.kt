86ea0337214eb64651452935bbb2845660c45952
Katz100 <cody.hopkins108@gmail.com>
2026-04-24 11:56:00 -0700

    Create fake repository

--- utility/src/main/java/com/example/utility/data/MetricsRepository.kt
+++ utility/src/main/java/com/example/utility/data/MetricsRepository.kt
@@ -5,8 +5,10 @@ import com.example.utility.data.db.FoodItem
 import com.example.utility.foreground.SessionState
 import com.example.utility.foreground.StepCountProvider
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 // Move to utility
@@ -79,5 +81,77 @@ class MetricsRepositoryImpl @Inject constructor(
 
     override suspend fun setNewWeight(weight: Int) {
         dataStore.setNewWeight(weight)
+    }
+}
+
+class MetricsRepositoryFake: MetricsRepository {
+    private val _stepTaken = MutableStateFlow(5000)
+    override val stepTaken: StateFlow<Int> = _stepTaken
+
+    private val _caloriesBurned = MutableStateFlow(50)
+    override val caloriesBurned: StateFlow<Int> = _caloriesBurned
+
+    private val _sessionState = MutableStateFlow(SessionState.STOPPED)
+    override val sessionState: StateFlow<SessionState> = _sessionState
+
+    private val _caloriesConsumed = MutableStateFlow<Int>(200)
+    override val caloriesConsumed: StateFlow<Int> = _caloriesConsumed
+
+    override val caloriesConsumedGoal: Flow<Int> = flowOf(2000)
+    override val weight: Flow<Int> = flowOf(180)
+    override val calorieFlow: Flow<Int> = flowOf(200)
+    override val stepGoal: Flow<Int> = flowOf(10_000)
+    override val currentDayFoodItems: Flow<List<FoodItem>> = flowOf(listOf(FoodItem(foodName = "Pizza", calories = 300),
+        FoodItem(foodName = "Burger", calories = 500)))
+
+    override fun caloriesConsumedProgress(transform: (Int, Int) -> Float): Flow<Float> {
+        return combine(caloriesConsumedGoal, currentDayFoodItems) { caloriesGoal, foodItems ->
+            val consumed = foodItems.sumOf { foodItem -> foodItem.calories }
+            transform(consumed, caloriesGoal)
+        }
+    }
+
+    override fun stepGoalProgress(transform: (Int, Int) -> Float): Flow<Float> {
+        return combine(stepGoal, stepTaken) {goal, current ->
+            transform(current, goal)
+        }
+    }
+
+    override fun calorieProgress(transform: (Int, Int) -> Float): Flow<Float> {
+        return combine(calorieFlow, caloriesBurned) { calorieGoal, caloriesBurnt ->
+            transform(caloriesBurnt, calorieGoal)
+        }
+    }
+
+    override suspend fun decrementKey(
+        key: Preferences.Key<Int>,
+        decrementValue: Int
+    ) {
+    }
+
+    override suspend fun incrementKey(
+        key: Preferences.Key<Int>,
+        incrementValue: Int
+    ) {
+    }
+
+    override suspend fun setNewWeight(weight: Int) {
+    }
+
+
+    fun setSteps(value: Int) {
+        _stepTaken.value = value
+    }
+
+    fun setCaloriesBurned(value: Int) {
+        _caloriesBurned.value = value
+    }
+
+    fun setSessionState(value: SessionState) {
+        _sessionState.value = value
+    }
+
+    fun setCaloriesConsumed(value: Int) {
+        _caloriesConsumed.value = value
     }
 }
