6cfaa66010965894e605592f09d190cd28a08538
Katz100 <cody.hopkins108@gmail.com>
2026-04-25 10:56:30 -0700

    Add scaffold to home page

--- feature/home/src/main/java/com/example/feature_home/HomePage.kt
+++ feature/home/src/main/java/com/example/feature_home/HomePage.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,13 +16,19 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -41,6 +48,7 @@ import com.example.utility.composables.StepsTakenCard
 import com.example.utility.foreground.SessionState
 import timber.log.Timber
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(
     modifier: Modifier = Modifier,
@@ -60,100 +68,101 @@ fun HomePage(
     val sessionActive = viewModel.sessionActive.collectAsState().value
     val caloriesConsumedGoal = viewModel.caloriesConsumedGoal.collectAsState().value
 
-    LaunchedEffect(sessionActive) {
-        Timber.i("Session State: $sessionActive")
-    }
 
-    LaunchedEffect(caloriesConsumed) {
-        Timber.i("Calories consumed: $caloriesConsumed")
-    }
-
-    LaunchedEffect(currentDayFoodItems) {
-        Timber.i("Current day food items: $currentDayFoodItems")
-    }
-
-    Column(
-        modifier = modifier.fillMaxSize()
-            .verticalScroll(scrollState),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(50.dp)
-    ) {
-        CustomCircularProgressIndicator(
-            modifier = Modifier.size(220.dp),
-            progress = stepProgress,
-            progressColor = colorResource(R.color.accent),
-            trackColor = colorResource(R.color.trackColor),
-            value = stepsTaken,
-            labelText = "STEPS"
-        )
-
-        // Replace icon with some calorie icon
-        ProgressCard(
-            modifier = Modifier.size(width = 300.dp, height = 200.dp),
-            value = caloriesBurned,
-            titleText = "BURNED",
-            titleTextStyle = TextStyle(
-                fontSize = 14.sp,
-                fontWeight = FontWeight.SemiBold,
-                letterSpacing = 1.5.sp,
-            ),
-            valueTextStyle = TextStyle(
-                color = colorResource(R.color.accent),
-                fontSize = 34.sp,
-                fontWeight = FontWeight.ExtraBold,
-                lineHeight = 36.sp
-            ),
-            icon = painterResource(R.drawable.walking),
-            iconTint = colorResource(R.color.accent),
-            progress = calorieProgress,
-            trackColor = colorResource(R.color.trackColor),
-            progressColor = colorResource(R.color.accent)
-        )
-        ProgressCard(
-            modifier = Modifier.size(width = 300.dp, height = 200.dp),
-            value = caloriesConsumed,
-            titleText = "CALORIES CONSUMED",
-            titleTextStyle = TextStyle(
-                fontSize = 14.sp,
-                fontWeight = FontWeight.SemiBold,
-                letterSpacing = 1.5.sp,
-            ),
-            valueTextStyle = TextStyle(
-                color = colorResource(R.color.lightGreenColor),
-                fontSize = 34.sp,
-                fontWeight = FontWeight.ExtraBold,
-                lineHeight = 36.sp,
-            ),
-            icon = painterResource(R.drawable.fork),
-            iconTint = colorResource(R.color.lightGreenColor),
-            progress = caloriesConsumedGoal,
-            trackColor = colorResource(R.color.trackColor),
-            progressColor = colorResource(R.color.accent)
-        )
-
-        Row(
-            modifier = Modifier.width(300.dp),
-            horizontalArrangement = Arrangement.Center
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = { TopAppBar(
+            title = {
+                Text("Activity")
+            },
+            windowInsets = WindowInsets(0, 0, 0, 0)
+        ) },
+        contentWindowInsets = WindowInsets(0, 0, 0, 0)
+    ) { innerPadding ->
+        Column(
+            modifier = modifier.fillMaxSize()
+                .padding(innerPadding)
+                .verticalScroll(scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(50.dp)
         ) {
-            LogFoodCard(
-                modifier = Modifier.size(width = 120.dp, height = 100.dp),
-                onClick = onLogFoodClicked
+            CustomCircularProgressIndicator(
+                modifier = Modifier.size(220.dp),
+                progress = stepProgress,
+                progressColor = colorResource(R.color.accent),
+                trackColor = colorResource(R.color.trackColor),
+                value = stepsTaken,
+                labelText = "STEPS"
             )
-            Spacer(modifier = Modifier.weight(1f))
-            StartSessionCard(
-                modifier = Modifier.size(width = 120.dp, height = 100.dp),
-                onClick = {
-                    onStartWalkClicked(weight, stepGoal, sessionActive)
-                          },
-                sessionActive = ((sessionActive == SessionState.RESUME) || (sessionActive == SessionState.PAUSE))
+
+            // Replace icon with some calorie icon
+            ProgressCard(
+                modifier = Modifier.size(width = 300.dp, height = 200.dp),
+                value = caloriesBurned,
+                titleText = "BURNED",
+                titleTextStyle = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    letterSpacing = 1.5.sp,
+                ),
+                valueTextStyle = TextStyle(
+                    color = colorResource(R.color.accent),
+                    fontSize = 34.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    lineHeight = 36.sp
+                ),
+                icon = painterResource(R.drawable.walking),
+                iconTint = colorResource(R.color.accent),
+                progress = calorieProgress,
+                trackColor = colorResource(R.color.trackColor),
+                progressColor = colorResource(R.color.accent)
             )
-        }
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth()
-                .height(200.dp)
-        ) {
-            items(currentDayFoodItems, key = { it.id.toString() }) { foodItem ->
-                Text("Food name: ${foodItem.foodName} Calories: ${foodItem.calories}")
+            ProgressCard(
+                modifier = Modifier.size(width = 300.dp, height = 200.dp),
+                value = caloriesConsumed,
+                titleText = "CALORIES CONSUMED",
+                titleTextStyle = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    letterSpacing = 1.5.sp,
+                ),
+                valueTextStyle = TextStyle(
+                    color = colorResource(R.color.lightGreenColor),
+                    fontSize = 34.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    lineHeight = 36.sp,
+                ),
+                icon = painterResource(R.drawable.fork),
+                iconTint = colorResource(R.color.lightGreenColor),
+                progress = caloriesConsumedGoal,
+                trackColor = colorResource(R.color.trackColor),
+                progressColor = colorResource(R.color.accent)
+            )
+
+            Row(
+                modifier = Modifier.width(300.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                LogFoodCard(
+                    modifier = Modifier.size(width = 120.dp, height = 100.dp),
+                    onClick = onLogFoodClicked
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                StartSessionCard(
+                    modifier = Modifier.size(width = 120.dp, height = 100.dp),
+                    onClick = {
+                        onStartWalkClicked(weight, stepGoal, sessionActive)
+                    },
+                    sessionActive = ((sessionActive == SessionState.RESUME) || (sessionActive == SessionState.PAUSE))
+                )
+            }
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth()
+                    .height(200.dp)
+            ) {
+                items(currentDayFoodItems, key = { it.id.toString() }) { foodItem ->
+                    Text("Food name: ${foodItem.foodName} Calories: ${foodItem.calories}")
+                }
             }
         }
     }
